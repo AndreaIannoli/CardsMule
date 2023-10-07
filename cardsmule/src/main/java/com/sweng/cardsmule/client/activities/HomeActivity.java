f8871288e70c544837ff64d0fa49b8bc5afc4b2f
MarcoMoro10 <marco.morelli15@studio.unibo.it>
2023-10-07 15:47:58 +0200

    Fixxing Bar pt2

--- cardsmule/src/main/java/com/sweng/cardsmule/client/activities/HomeActivity.java
+++ cardsmule/src/main/java/com/sweng/cardsmule/client/activities/HomeActivity.java
@@ -3,7 +3,7 @@ package com.sweng.cardsmule.client.activities;
 
 import com.sweng.cardsmule.client.authentication.User;
 import com.sweng.cardsmule.shared.models.*;
-
+import com.sweng.cardsmule.client.BaseAsyncCallback;
 import com.sweng.cardsmule.client.views.HomeView;
 import com.sweng.cardsmule.shared.CardServiceAsync;
 import com.google.gwt.activity.shared.AbstractActivity;
@@ -14,17 +14,19 @@ import com.google.gwt.user.client.ui.AcceptsOneWidget;
 import java.util.List;
 
 public class HomeActivity extends AbstractActivity implements HomeView.Presenter {
+    private final CardServiceAsync rpcService;
     private final HomeView view;
     private final User user;
     private final PlaceController placeController;
-   
+    private List<SwengCard> card;
     
 
 
-    public HomeActivity(HomeView view,User user,PlaceController placeController) {
+    public HomeActivity(HomeView view,User user,PlaceController placeController, CardServiceAsync rpcService) {
         this.view = view;
         this.user = user;
         this.placeController = placeController;
+        this.rpcService=rpcService;
         
     }
 
@@ -49,13 +51,19 @@ public class HomeActivity extends AbstractActivity implements HomeView.Presenter
 		if(game == null) {
     		throw new IllegalArgumentException("game cannot be null");
     	}
-    	/*rpcService.getGameCards(game, new BaseAsyncCallback<List<SwengCards>>() {
+    	rpcService.getGameCards(game, new BaseAsyncCallback<List<SwengCard>>() {
             @Override
-            public void onSuccess(List<SwengCards> result) {
-                view.setData(result);
-                cards = result;
+            public void onSuccess(List<SwengCard> result) {
+            	//All'interno del callback, nell'implementazione del metodo onSuccess(List<Card> result), 
+            	//i risultati ottenuti dalla chiamata al servizio vengono passati alla vista (view.setData(result)) 
+            	//per aggiornare la visualizzazione con le nuove carte ottenute.
+                view.setAttributesAndtypes(result);
+                //La linea cards = result; assegna la lista di carte ottenute dalla chiamata al servizio remoto 
+                //(contenute nella variabile result) alla variabile cards. Questa assegnazione sovrascrive il valore 
+                //precedente di cards con la nuova lista di carte ottenute dalla chiamata al servizio
+                card = result;
             }
-        });*/
+        });
 		
 	}
 }
